2f0bcd4f285ab98f0add9dac9d74b323de2bf434
Ned Faulhaber <nedfaulhaber@gmail.com>
2021-06-10 03:38:27 -0700

    more changes and began artist pgs

---
+++ js/maalem-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("maalem-mobile-nav").style.width = "250px";
+    document.getElementById("maalem-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("maalem-mobile-nav").style.width = "0";
+    document.getElementById("maalem-wrapper").style.marginRight = "0";
+}
