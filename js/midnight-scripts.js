933c358e5226e6a72d8e2a9c944cbad32390425b
Ned Faulhaber <nedfaulhaber@gmail.com>
2021-06-14 00:55:46 -0700

    set up husker and midnight pages

---
+++ js/midnight-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("midnight-mobile-nav").style.width = "250px";
+    document.getElementById("midnight-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("midnight-mobile-nav").style.width = "0";
+    document.getElementById("midnight-wrapper").style.marginRight = "0";
+}
