ea43e145c5ee63107f013eaafe8eea14b60dd020
Ned Faulhaber <nedfaulhaber@gmail.com>
2021-07-01 01:10:33 -0700

    elida page set up

---
+++ js/elida-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("elida-mobile-nav").style.width = "250px";
+    document.getElementById("elida-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("elida-mobile-nav").style.width = "0";
+    document.getElementById("elida-wrapper").style.marginRight = "0";
+}
