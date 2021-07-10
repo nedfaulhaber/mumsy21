ceae9be674f393a94279a4bac05ecd7a01e8bff6
Ned Faulhaber <nedfaulhaber@gmail.com>
2021-07-10 00:54:41 -0700

    finished sophie skeleton

---
+++ js/sophie-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sophie-mobile-nav").style.width = "250px";
+    document.getElementById("sophie-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sophie-mobile-nav").style.width = "0";
+    document.getElementById("sophie-wrapper").style.marginRight = "0";
+}
