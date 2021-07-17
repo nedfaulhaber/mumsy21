d2806832aed9fa896aeda106e08428adfc388992
Ned Faulhaber <nedfaulhaber@gmail.com>
2021-07-17 00:28:02 -0700

    changed elida color palette; created orville

--- js/maren-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("maren-mobile-nav").style.width = "250px";
-    document.getElementById("maren-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("maren-mobile-nav").style.width = "0";
-    document.getElementById("maren-wrapper").style.marginRight = "0";
-}
