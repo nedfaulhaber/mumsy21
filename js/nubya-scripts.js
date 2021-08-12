87ec7f22b60ac2bcd224fd5c3e3bf5e0023331f4
Ned Faulhaber <nedfaulhaber@gmail.com>
2021-08-12 01:31:05 -0700

    finished nubya page

---
+++ js/nubya-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("nubya-mobile-nav").style.width = "250px";
+    document.getElementById("nubya-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("nubya-mobile-nav").style.width = "0";
+    document.getElementById("nubya-wrapper").style.marginRight = "0";
+}
