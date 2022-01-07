6fa181f1cd879e898a282e77af3ddfbf1724237d
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-07 00:34:52 -0800

    oklou all but text

--- js/midnight-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("midnight-mobile-nav").style.width = "250px";
-    document.getElementById("midnight-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("midnight-mobile-nav").style.width = "0";
-    document.getElementById("midnight-wrapper").style.marginRight = "0";
-}
---
+++ js/oklou-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("oklou-mobile-nav").style.width = "250px";
+    document.getElementById("oklou-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("oklou-mobile-nav").style.width = "0";
+    document.getElementById("oklou-wrapper").style.marginRight = "0";
+}
