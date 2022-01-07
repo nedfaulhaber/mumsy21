de4bcce68604b6d28f1c7c76c608b36647a9068f
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-07 00:53:53 -0800

    moses all but text

--- js/elida-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("elida-mobile-nav").style.width = "250px";
-    document.getElementById("elida-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("elida-mobile-nav").style.width = "0";
-    document.getElementById("elida-wrapper").style.marginRight = "0";
-}
---
+++ js/moses-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("moses-mobile-nav").style.width = "250px";
+    document.getElementById("moses-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("moses-mobile-nav").style.width = "0";
+    document.getElementById("moses-wrapper").style.marginRight = "0";
+}
