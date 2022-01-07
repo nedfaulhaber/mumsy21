102cf461f845f48291042b99afd52c38e5fa9865
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-07 00:06:15 -0800

    osterreich all but text

--- js/husker-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("husker-mobile-nav").style.width = "250px";
-    document.getElementById("husker-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("husker-mobile-nav").style.width = "0";
-    document.getElementById("husker-wrapper").style.marginRight = "0";
-}
---
+++ js/osterreich-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("osterreich-mobile-nav").style.width = "250px";
+    document.getElementById("osterreich-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("osterreich-mobile-nav").style.width = "0";
+    document.getElementById("osterreich-wrapper").style.marginRight = "0";
+}
