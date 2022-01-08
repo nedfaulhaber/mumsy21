4d6de8e24c9282f78ac36cbd7c01024266750635
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-08 03:15:16 -0800

    animals as leaders all but text

---
+++ js/animals-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("animals-mobile-nav").style.width = "250px";
+    document.getElementById("animals-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("animals-mobile-nav").style.width = "0";
+    document.getElementById("animals-wrapper").style.marginRight = "0";
+}
--- js/orville-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("orville-mobile-nav").style.width = "250px";
-    document.getElementById("orville-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("orville-mobile-nav").style.width = "0";
-    document.getElementById("orville-wrapper").style.marginRight = "0";
-}
