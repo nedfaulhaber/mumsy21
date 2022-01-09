4b1bf31839bed26ef65272828811e4fae2fd2334
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-09 00:28:21 -0800

    number one popstar all but text

--- js/caroline-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("caroline-mobile-nav").style.width = "250px";
-    document.getElementById("caroline-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("caroline-mobile-nav").style.width = "0";
-    document.getElementById("caroline-wrapper").style.marginRight = "0";
-}
