29ff754659390188bc4fe3533afedf36f3d447ff
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-09 00:07:52 -0800

    porches all but text

---
+++ js/porches-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("porches-mobile-nav").style.width = "250px";
+    document.getElementById("porches-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("porches-mobile-nav").style.width = "0";
+    document.getElementById("porches-wrapper").style.marginRight = "0";
+}
