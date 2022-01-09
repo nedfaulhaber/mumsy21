9d44d14e170421bbc6d8e5c86aeed7723b9aeb8f
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-09 01:27:31 -0800

    la joaqui all but text

---
+++ js/lajoaqui.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("lajoaqui-mobile-nav").style.width = "250px";
+    document.getElementById("lajoaqui-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("lajoaqui-mobile-nav").style.width = "0";
+    document.getElementById("lajoaqui-wrapper").style.marginRight = "0";
+}
