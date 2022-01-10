82bbd69b19a231a52f0bd59dfa2cdae4f5bdde82
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-10 01:26:02 -0800

    remi all but text

---
+++ js/remi-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("remi-mobile-nav").style.width = "250px";
+    document.getElementById("remi-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("remi-mobile-nav").style.width = "0";
+    document.getElementById("remi-wrapper").style.marginRight = "0";
+}
--- js/toro-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("toro-mobile-nav").style.width = "250px";
-    document.getElementById("toro-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("toro-mobile-nav").style.width = "0";
-    document.getElementById("toro-wrapper").style.marginRight = "0";
-}
