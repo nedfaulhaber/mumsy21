407430d7f8c512434fc2dd4c7f1650b2bb26e366
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-10 02:11:03 -0800

    terje all but text

---
+++ js/terje-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("terje-mobile-nav").style.width = "250px";
+    document.getElementById("terje-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("terje-mobile-nav").style.width = "0";
+    document.getElementById("terje-wrapper").style.marginRight = "0";
+}
--- js/twigs-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("twigs-mobile-nav").style.width = "250px";
-    document.getElementById("twigs-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("twigs-mobile-nav").style.width = "0";
-    document.getElementById("twigs-wrapper").style.marginRight = "0";
-}
