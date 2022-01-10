1b6bf2c55c4eb0e3a49b43df8158d4b15da0fa45
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-10 01:06:01 -0800

    golden gate jubilee quartet all but text

---
+++ js/golden-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("golden-mobile-nav").style.width = "250px";
+    document.getElementById("golden-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("golden-mobile-nav").style.width = "0";
+    document.getElementById("golden-wrapper").style.marginRight = "0";
+}
