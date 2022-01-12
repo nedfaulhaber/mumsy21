38463c7929e71aca42efa7416efb1009adbc8120
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-12 00:25:36 -0800

    bazan all but text

---
+++ js/bazan-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("bazan-mobile-nav").style.width = "250px";
+    document.getElementById("bazan-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("bazan-mobile-nav").style.width = "0";
+    document.getElementById("bazan-wrapper").style.marginRight = "0";
+}
--- js/nils-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("nils-mobile-nav").style.width = "250px";
-    document.getElementById("nils-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("nils-mobile-nav").style.width = "0";
-    document.getElementById("nils-wrapper").style.marginRight = "0";
-}
