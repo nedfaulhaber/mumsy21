b4dee1e71062741367e2574577846a533d594f3e
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-12 00:58:21 -0800

    bjork all but text

---
+++ js/bjork-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("bjork-mobile-nav").style.width = "250px";
+    document.getElementById("bjork-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("bjork-mobile-nav").style.width = "0";
+    document.getElementById("bjork-wrapper").style.marginRight = "0";
+}
--- js/rush2-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("rush2-mobile-nav").style.width = "250px";
-    document.getElementById("rush2-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("rush2-mobile-nav").style.width = "0";
-    document.getElementById("rush2-wrapper").style.marginRight = "0";
-}
