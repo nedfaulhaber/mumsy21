3b2155495962f77ea7894c0fc3ff8206f6829f41
Ned Faulhaber <nedfaulhaber@gmail.com>
2022-01-12 00:04:47 -0800

    mingus all but text

---
+++ js/mingus-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mingus-mobile-nav").style.width = "250px";
+    document.getElementById("mingus-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mingus-mobile-nav").style.width = "0";
+    document.getElementById("mingus-wrapper").style.marginRight = "0";
+}
